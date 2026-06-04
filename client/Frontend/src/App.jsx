c14548f5266b3a2c1d61f389750afc2e45cff847
Ayush6950 <ayushmalviya9294@gmail.com>
2026-06-04 23:34:22 +0530

    vite-tailwind-postcss

--- client/Frontend/src/App.jsx
+++ client/Frontend/src/App.jsx
@@ -1,5 +1,11 @@
 function App() {
-<></>
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline  bg-amber-300">
+        Hello world!
+      </h1>
+    </div>
+  )
 }
 
 export default App
