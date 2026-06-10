80efb172da44c7c64b17c31b4be137d080ef78fa
Ayush6950 <ayushmalviya9294@gmail.com>
2026-06-11 02:41:21 +0530

    ..

--- server/routes/user.route.js
+++
@@ -1,9 +0,0 @@
-import isAuth from "../middleware/isAuth";
-import express from express;
-import { getCurrentUser } from "../controllers/user.controllers";
-
-const userRouter = express.Router()
-
-userRouter.get("/currentuser", isAuth,getCurrentUser);
-
-export default userRouter;  
