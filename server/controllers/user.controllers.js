91c266a014fc41b6cb98196e382ac16b8a0c1e26
Ayush6950 <ayushmalviya9294@gmail.com>
2026-06-21 14:48:56 +0530

    user-controller

--- server/controllers/user.controllers.js
+++ server/controllers/user.controllers.js
@@ -7,6 +7,14 @@ export const getCurrentUser = async (req,res,) =>  {
         if(!user){
             return res.status(404).json({message:"Current User is not found"})
         }
+        
+        // Auto top-up credits to 50 if they fall below 10 (for development/testing convenience)
+        if (user.credits < 10) {
+            user.credits = 50;
+            user.isCreditAvailable = true;
+            await user.save();
+        }
+
         return res.status(200).json(user)
         }catch(error){
             return res.status(500).json({message:`getCurrentUser error  ${error}`})
